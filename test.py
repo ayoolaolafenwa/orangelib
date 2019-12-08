38ca63e303eaad0f660823c456760d1c4e6b5664
ayoolaolafenwa <52606857+ayoolaolafenwa@users.noreply.github.com>
2019-12-08 03:10:13 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,11 @@
 #Having installed your library in the step above. Create a new file below anywhere on your computer and write the following code to predict the image of a fruit.
 
 
-from orangelib.model import FruitClassifier
+from orangelib.model import OrangeClassifier
 
-classifier = FruitClassifier("orange_model_96.h5")
+classifier = FruitClassifier("path_to_trained_model")
 
-fruit_name, confidence = classifier.predict("sample.jpg")
+fruit_name, confidence = classifier.predict("path_to_image")
 
 print(" Fruit Name: ",fruit_name)
 print("Prediction Confidence: ",confidence)
